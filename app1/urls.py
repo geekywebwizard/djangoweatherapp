edec0221cf68ddf1b94bbccda02e03708bf6705e
Rakesh Kumar Tadlapur <rakesh@geekywebwizards.com>
2021-09-30 20:27:46 +0530

    Integrated OpenWeatherMap API

--- app1/urls.py
+++ app1/urls.py
@@ -1,11 +1,13 @@
+from os import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
 
- path( '', views.home, name='home'),
+ path( '', views.openweather, name='home'),
  path( 'home', views.home, name="home"),
  path('about', views.about, name='about'),
- path('contact', views.contact, name='contact')
+ path('contact', views.contact, name='contact'),
+ path('open', views.openweather, name='open')
 
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,4 @@
+from io import RawIOBase
 from django.shortcuts import render
 from requests.sessions import Request
 from datetime import date, datetime
@@ -11,7 +12,8 @@ def home(request):
     if request.method  == "POST":
         zipcode = request.POST["zipcode"]
         today = datetime.today
-        api_request = requests.get("https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode="+zipcode+"&date=2021-09-30&distance=50&API_KEY=B176FE17-0119-4E2F-A142-5F74A516CF97")
+        api_request = requests.get("https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode="+zipcode+"&date=&distance=50&API_KEY=B176FE17-0119-4E2F-A142-5F74A516CF97")
+        
             
         try:
             api = json.loads(api_request.content)
@@ -40,4 +42,32 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html', {})
+
+
+def openweather(request):
+
+    import json
+    import requests
+
+    if request.method  == "POST":
+        zipcode_open = request.POST["zipcode_open"]
+        country_open = request.POST["country_open"]
+
+
+        open_api_request = requests.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipcode_open+","+country_open+"&appid=0e4c021e29f746ddd6e9c1a8764f2a0f")
+        
+        open_api = json.loads(open_api_request.content)
+        
+        return render(request, 'openweather.html' , {'open_api': open_api} )
+
+
+    else:
+
+        open_api_request = requests.get("http://api.openweathermap.org/data/2.5/weather?zip=585105,IN&appid=0e4c021e29f746ddd6e9c1a8764f2a0f")
+        
+        open_api = json.loads(open_api_request.content)
+        
+        return render(request, 'openweather.html' , {'open_api': open_api} )
+
+    
     
