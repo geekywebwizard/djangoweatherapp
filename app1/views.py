14dd09abd1103cda8bc252c3720b3cc276afdb2d
Rakesh Kumar Tadlapur <rakesh@geekywebwizards.com>
2021-09-30 11:02:16 +0530

    AirNow API Integrated

--- app1/views.py
+++ app1/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html' , {} )
+    #https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=35242&date=2021-09-30&distance=25&API_KEY=B176FE17-0119-4E2F-A142-5F74A516CF97
+    
+    import json
+    import requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=35242&date=2021-09-30&distance=25&API_KEY=B176FE17-0119-4E2F-A142-5F74A516CF97")
+    
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error ..."
+
+    
+    return render(request, 'home.html' , {'api': api} )
 
 def about(request):
     return render(request, 'about.html', {})
